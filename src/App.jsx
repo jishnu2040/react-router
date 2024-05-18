1f691f33102643e2221e158fa6ff9c15d01dc81b
jishnuraj rs <jishnuraj017@gmail.com>
2024-05-18 15:46:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1 className='bg-yellow-500 text-blue-500'>React router dom</h1>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/GitHub/GitHub.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GitHub() {
+  return (
+    <div>
+      <h1>GitHub</h1>
+    </div>
+  )
+}
+
+export default GitHub
